4f0f79e5a367541f5648a280c0859aa1f224850c
Carlos <carlos.av.amorim@gmail.com>
2022-07-07 20:30:35 -0300

    CHORE: API log

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import { useFirstPrismicDocument } from "@prismicio/react";
 import React from "react";
 
+
 function App() {
+
+  const [document] = useFirstPrismicDocument()
+
+  console.log(document)
+
   return (
     <h1>Teste</h1>
   );
